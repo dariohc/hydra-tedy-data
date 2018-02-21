d8c026cf881c90cc9455f71428183ca9d498834f
Dario Hermida <dariohc@gmail.com>
2018-02-21 23:17:01 +0100

    working file for data exported from control room
    works for LMS 14a hydra control

--- hydra-tedy-data-script.py
+++ hydra-tedy-data-script.py
@@ -1,14 +1,74 @@
-file = open("10-test-I-TVC-Sine-Z.unv", "r")
-file_out = open('output.txt', 'w')
-search_string = "Time for "
-for k in range(1000000):
-   current_line = file.readline()
-   if current_line.rfind(search_string) >= 0:
-        file_out.write(current_line[len(search_string):])
-        print ("I'm here writing the sensor")
-        for j in range(4) : file.readline()
-        my_list = list(file.readline().split())
-        file_out.write(my_list[5] + '\n\n')
-
-file.close()
-file_out.close()
+input_list = ["Facility data/6-test-I-Sine-Y/Sine_1/6-test-I-AGE-Sine-Y.unv", \
+              "Facility data/7-test-I-Sine-Z/Sine_2/7-test-I-AGE-Sine-Z.unv", \
+              "Facility data/8-test-I-TVC-Sine-X/Sine_2/8-test-I-TVC-Sine-X.unv", \
+              "Facility data/9-test-I-TVC-Sine-Y/Sine_1/9-test-I-TVC-Sine-Y.unv", \
+              "Facility data/10-test-I-TVC-Sine-Z/Sine_1/10-test-I-TVC-Sine-Z.unv", \
+"Facility data/11-test-I-AIC-Sine-X/Sine_8/11-test-I-AIC-Sine-X.unv", \
+"Facility data/12-test-A-bis-Transient/RanRed_2_25percent/12-test-A-bis-25percent.unv", \
+"Facility data/12-test-A-bis-Transient/RanRed_3_50percent/12-test-A-bis-50percent.unv", \
+"Facility data/12-test-A-bis-Transient/RanRed_4_100percent/12-test-A-bis-100percent.unv", \
+"Facility data/13-test-B04-B11-Transient/RanRed_2_25percent/13-test-B04-B11-25percent.unv", \
+"Facility data/13-test-B04-B11-Transient/RanRed_3_50percent/13-test-B04-B11-50percent.unv", \
+"Facility data/13-test-B04-B11-Transient/RanRed_4_100percent/13-test-B04-B11-100percent.unv", \
+"Facility data/14-test-D-Transient/RanRed_1_5percent/14-test-D-5percent.unv", \
+"Facility data/14-test-D-Transient/RanRed_2_10percent/14-test-D-10percent.unv", \
+"Facility data/14-test-D-Transient/RanRed_3_15percent/14-test-D-15percent.unv",\
+"Facility data/15-test-C-Transient/RanRed_1_25percent/15-test-C-25percent.unv",\
+"Facility data/15-test-C-Transient/RanRed_2_100percent/15-test-C-transient-100percent.unv",\
+"Facility data/16-test-B12-B17-Transient/RanRed_1_25percent/16-test-B12-B17-Transient-25percent.unv",\
+"Facility data/16-test-B12-B17-Transient/RanRed_2_50percent/16-test-B12-B17-Transient-50percent.unv",\
+"Facility data/16-test-B12-B17-Transient/RanRed_3_100percent/16-test-B12-B17-Transient-100percent.unv",\
+"Facility data/17-test-B18-B21-Transient/RanRed_1_10percent/17-test-B18-B21-Transient-10percent.unv",\
+"Facility data/17-test-B18-B21-Transient/RanRed_2_25percent/17-test-B18-B21-Transient-25percent.unv",\
+"Facility data/17-test-B18-B21-Transient/RanRed_3_50percent/17-test-B18-B21-Transient-50percent.unv",\
+"Facility data/18-test-I-AIC-Sine-Y/Sine_1/18-test-I-AIC-Sine-Y.unv",\
+"Facility data/19-test-I-AIC-Sine-Z/Sine_1/19-test-I-AIC-Sine-Z.unv",\
+"Facility data/2-pretestX-FMD-empty/Sine_1/2-pretest-empty-AGE-Sine-X.unv",\
+"Facility data/20-test-B1-AIC-Sine-X/Sine_3/20-test-B1-Sine-X.unv",\
+"Facility data/22-test-B3-AIC-Sine-Z/Sine_1/22-test-B3-AIC-Sine-Z.unv",\
+"Facility data/23-test-I-AIC2-Sine-X/Sine_1_Sm1_DA200/23-test-I-AIC2bis-SineX.unv",\
+"Facility data/23-test-I-AIC2-Sine-X/Sine_2_SM8_DA20/23-test-I-AIC2-SineX.unv",\
+"Facility data/24-test-I-AIC2-Sine-Y/Sine_3_NOK/24-test-I-AIC2-SineY.unv",\
+"Facility data/25-test-I-AIC3-Sine-Y/Sine_1/25-test-I-AIC3-SineY.unv",\
+"Facility data/26-test-I-AIC3-Sine-Z/Sine_1/26-test-I-AIC3-SineZ.unv",\
+"Facility data/27-test-I-AIC3-Sine-X/Sine_1/27-test-I-AIC3-SineX.unv",\
+"Facility data/28-test-I-AIC4-Sine-X/Sine_1/28-test-I-AIC4-SineX.unv",\
+"Facility data/29-test-E-Transient/RanRed_1_25percent/29-test-E-Transient-25percent.unv",\
+"Facility data/29-test-E-Transient/RanRed_2_50percent/29-test-E-Transient-50percent.unv",\
+"Facility data/29-test-E-Transient/RanRed_3_100percent/29-test-E-Transient-100percent.unv",\
+"Facility data/3-pretestY-FMD-empty/Sine_1/3-pretest-empty-AGE-Sine-Y.unv",\
+"Facility data/30-test-I-AIC4-Sine-Y/Sine_1/30-test-I-AIC4-Sine-Y.unv",\
+"Facility data/31-test-I-AIC4-Sine-Z/Sine_1/31-test-I-AIC4-Sine-Z.unv",\
+"Facility data/32-test-F-Transient/RanRed_1_25percent/32-test-F-Transient-25percent.unv",\
+"Facility data/32-test-F-Transient/RanRed_2_50percent/32-test-F-Transient-50percent.unv",\
+"Facility data/32-test-F-Transient/RanRed_3_100percent/32-test-F-Transient-100percent.unv",\
+"Facility data/4-pretestZ-FMD-empty/Sine_1/4-pretest-empty-AGE-Sine-Z.unv",\
+"Facility data/5-test-I-Sine-X/Sine_2/5-test-I-AGE-Sine-X.unv",\
+"Facility data/6-test-I-Sine-Y/Sine_1/6-test-I-AGE-Sine-Y.unv",\
+"Facility data/7-test-I-Sine-Z/Sine_2/7-test-I-AGE-Sine-Z.unv",\
+"Facility data/8-test-I-TVC-Sine-X/Sine_2/8-test-I-TVC-Sine-X.unv",\
+"Facility data/9-test-I-TVC-Sine-Y/Sine_1/9-test-I-TVC-Sine-Y.unv",\
+"Facility data/1-pretest-online-random/RanRed_3/pretest_test_A_50percent.unv",\
+"Facility data/1-pretest-online-random/RanRed_4/pretest_test_A_100percent.unv"
+             ]
+for input_file in input_list:
+    input_string_file = str(input_file)
+    file = open(input_string_file, "r")
+    file_out = open(input_string_file + '-output.txt', 'w')
+    search_string = "Time for "
+    sensor_counter = 0
+    # processing the universal file
+    #for k in range(1000000):
+    for line in file:
+        #current_line = file.readline()
+        current_line = str(line)
+        if current_line.rfind(search_string) >= 0:
+            sensor_counter += 1
+            file_out.write(str(sensor_counter) + ', ' + current_line[len(search_string):-1])
+            print("I'm here writing the sensor" + str(file_out))
+            for j in range(4): file.readline()
+            my_list = list(file.readline().split())
+            file_out.write(', ' + my_list[5] + '\n')
+    #process the output.txt file
+    file.close()
+    file_out.close()
